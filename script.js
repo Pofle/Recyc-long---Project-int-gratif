b119010070c15a6a59519f1a5abce6e8e1d004fb
Pofle <pauline.fleury42@gmail.com>
2022-12-11 15:43:30 +0100

    Created Information icon

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function pop_up(divIBAN){
+    var iban = document.getElementById('divIBAN');
+
+    iban.style.display = "block";
+}
+
+function cacherIban(divIBAN){
+    var iban =document.getElementById('divIBAN');
+
+    iban.style.display = 'none';
+}
