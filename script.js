b9cd709c1292a3c2b5ff11a68d4b1b26915ee6a5
Pofle <pauline.fleury42@gmail.com>
2022-12-11 17:46:32 +0100

    Create Pop-up avoid & both signup.html

--- script.js
+++ script.js
@@ -2,16 +2,28 @@
 
 
 function afficher_pop_up(recyc_question_icon){
-    const button = document.getElementById('recyc-question_icon');
-    var pop_up = document.getElementById('pop-up_box');
+    const help_recyc = document.getElementById('recyc-question_icon');
+    const help_avoir = document.getElementById('avoid-question_icon');
+    const help_both = document.getElementById('both-question_icon');
+    
+    var pop_up1 = document.getElementById('pop-up_box1');
+    var pop_up2 = document.getElementById('pop-up_box2');
+    var pop_up3 = document.getElementById('pop-up_box3');
 
-    button.addEventListener('click', event => { pop_up.style.display = "block"; });
+    help_recyc.addEventListener('click', event => { pop_up1.style.display = "block"; });
+    help_avoir.addEventListener('click', event => { pop_up2.style.display = "block"});
+    help_both.addEventListener('click', event => { pop_up3.style.display = "block"});
+
 }
 
 
 
 function cacher_pop_up(){
-    var pop_up =document.getElementById('pop-up_box');
+    var pop_up = document.getElementById('pop-up_box1');
+    var pop_up2 = document.getElementById('pop-up_box2');
+    var pop_up3 = document.getElementById('pop-up_box3');
 
     pop_up.style.display = 'none';
+    pop_up2.style.display = 'none';
+    pop_up3.style.display = 'none';
 }
